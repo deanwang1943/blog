b04cfa00fb9bac83e78c0ddc7a2cf26a10435538
deanwang1943 <wangjingxin1943@163.com>
2018-08-13 16:54:57 +0800

    airflow命令更新

--- push_build_devlop.sh
+++ push_build_devlop.sh
@@ -7,10 +7,10 @@ git commit -am "${COMMIT_MSG}"
 
 git push origin master
 
-echo wjx123 | sudo -S docker start dean-hexo
+sudo -S docker start dean-hexo
  
-echo wjx123 | sudo -S docker exec -it dean-hexo hexo clean && hexo g
+sudo -S docker exec -it dean-hexo hexo clean && hexo g
 
-echo wjx123 | sudo -S docker exec -it dean-hexo hexo d
+sudo -S docker exec -it dean-hexo hexo d
 
-echo wjx123 | sudo -S docker stop dean-hexo
+sudo -S docker stop dean-hexo
